3ab0aa24d5599c85e6cb08b51e87406045505c7c
alpedhuzes <58863999+alpedhuzes@users.noreply.github.com>
2020-02-08 12:33:15 +0100

    New setup

---
+++ script.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+	document.getElementById('contentextended').innerHTML = '<p>Hi, this is some additional HTML that just loads when the page is already complete.</p>';
+};
